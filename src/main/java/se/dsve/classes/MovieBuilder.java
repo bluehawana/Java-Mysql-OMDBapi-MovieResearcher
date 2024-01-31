267b4c09ea90ce11ce3716db40b9524b2c48271d
Lars Strömberg <lars@dsve.se>
2024-01-31 17:54:02 +0000

    Add MovieBuilder.java

---
+++ src/main/java/se/dsve/classes/MovieBuilder.java
@@ -0,0 +1,62 @@
+package se.dsve.classes;
+
+public class MovieBuilder {
+
+    private String title;
+    private int year;
+    private String actors;
+    private String director;
+    private String genre;
+
+    public MovieBuilder(String title, int year, String actors, String director, String genre) {
+        this.title = title;
+        this.year = year;
+        this.actors = actors;
+        this.director = director;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Movie build() {
+        return new Movie(title, year, actors, director, genre);
+    }
+}
